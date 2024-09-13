32aad4ec250d1599fb2e6e0ea41a8de8f95bf809
sarvesh <sarveshy@adobe.com>
2024-09-13 20:47:38 +0530

    Check

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    BASE_URL: process.env.BASE_URL,
-    PORT: process.env.PORT || "3000",
-    SLACK: {
-      CLIENT_ID: process.env.SLACK_CLIENT_ID,
-      CLIENT_SECRET: process.env.SLACK_CLIENT_SECRET
-    }
-  };
